b972b53fe770f390f3fca32c70e52608101eb121
USER <user@AL01467022.local>
2020-12-21 02:31:49 +0900

    GraphQL integration

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const next = require('next')
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  const server = express()
+
+  const schema = buildSchema(`
+    type Query {
+        hello: String
+    }
+  `);
+
+  const root = {
+
+  }
+
+  server.use('/graphql', graphqlHTTP({
+      schema,
+      rootValue: root,
+      graphiql: true,
+  }));
+
+  server.all('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
