2a546621f8af8cc6faeff6afae7575ae0f940744
USER <user@AL01467022.local>
2020-12-31 12:57:49 +0900

    Add apollo client

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,37 @@
-import App from 'next/app';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/index.scss';
 
 import Navbar from '@/components/shared/Navbar';
 import Hero from '../components/shared/Hero';
 
+const client = new ApolloClient({
+    uri: 'http://localhost:3000/graphql',
+    cache: new InMemoryCache()
+});
 
 const MyApp = ({Component, pageProps}) => {
 
     const isHomePage = () => Component.name === "Home"
 
     return (
-        <div className="portfolio-app">
-            <Navbar/>
-            {pageProps.appData}
-            {isHomePage() && <Hero/>}
-            <div className="container">
-                <Component {...pageProps}/>
-            </div>
-            {/* FOOTER STARTS */}
-            {isHomePage() && <footer id="sticky-footer" className="py-4 bg-black text-white-50 py-3">
-                <div className="container text-center">
-                <small>Copyright &copy; Your Website</small>
+        <ApolloProvider client={client}>
+            <div className="portfolio-app">
+                <Navbar/>
+                {pageProps.appData}
+                {isHomePage() && <Hero/>}
+                <div className="container">
+                    <Component {...pageProps}/>
                 </div>
-            </footer>}
-            {/* FOOTER ENDS */}
-        </div>
+                {/* FOOTER STARTS */}
+                {isHomePage() && <footer id="sticky-footer" className="py-4 bg-black text-white-50 py-3">
+                    <div className="container text-center">
+                    <small>Copyright &copy; Your Website</small>
+                    </div>
+                </footer>}
+                {/* FOOTER ENDS */}
+            </div>
+        </ApolloProvider>
     )
 }
 
