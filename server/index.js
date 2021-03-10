81790bb21c651bb35b0849b0dd1195f705be53fa
SEUNGHO LEE <shlee1353@gmail.com>
2021-03-10 13:55:21 +0900

    Separating apollo functions on server (#15)
    Co-authored-by: USER <user@AL01467022.local>

--- server/index.js
+++ server/index.js
@@ -1,58 +1,17 @@
 const express = require('express');
 const next = require('next');
-const mongoose = require('mongoose');
-
-const { ApolloServer, gql } = require('apollo-server-express');
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-const { portfolioQueries, portfolioMutations } = require('./graphql/resolvers');
-const { portfolioTypes } = require('./graphql/types');
-
-const Portfolio = require('./graphql/models/Portfolio');
-
 require('./database').connect();
 
 app.prepare().then(() => {
   const server = express()
 
-  // Construct a shcema, using GraphQl schema language
-  const typeDefs = gql`
-    ${portfolioTypes}
-
-    type Query {
-        portfolio(id: ID): Portfolio
-        portfolios: [Portfolio]
-    }
-
-    type Mutation {
-        createPortfolio(input: PortfolioInput): Portfolio
-        updatePortfolio(id: ID, input: PortfolioInput): Portfolio
-        deletePortfolio(id: ID): ID
-    }
-  `;
-
-  // The root provides a resolver for each API endpoint
-  const resolvers = {
-    Query: {
-      ...portfolioQueries
-    },
-    Mutation: {
-      ...portfolioMutations
-    }
-  }
-
-  const apolloServer = new ApolloServer({ 
-    typeDefs, resolvers,
-    context: () => ({
-      models: {
-        Portfolio: new Portfolio(mongoose.model('Portfolio'))
-      }
-    })
-  })
+  const apolloServer = require('./graphql').createApolloServer();
   apolloServer.applyMiddleware({ app: server })
 
   server.all('*', (req, res) => {
